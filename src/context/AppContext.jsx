795a2f67ee4ea2ee815d3df18e7d757285e768d5
Sonu Kumar <somammahi@gmail.com>
2025-01-09 19:16:41 +0530

    first commit

---
+++ src/context/AppContext.jsx
@@ -0,0 +1,91 @@
+import axios from "axios";
+import { createContext, useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
+
+export const AppContext = createContext();
+
+const AppContextProvider = (props) => {
+  const [user, setUser] = useState(null);
+  const [showLogin, setShowLogin] = useState(false);
+  const [token, setToken] = useState(localStorage.getItem("token"));
+
+  const [credit, setCredit] = useState(false);
+  const navigate = useNavigate();
+
+  const backendUrl = import.meta.env.VITE_BACKEND_URL;
+
+  const loadCreditsData = async () => {
+    try {
+      const { data } = await axios.get(backendUrl + "/api/user/credits", {
+        headers: { token },
+      });
+
+      if (data.success) {
+        setCredit(data.credits);
+        setUser(data.user);
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error(error.message);
+    }
+  };
+
+  const generateImage = async (prompt) => {
+    try {
+      const { data } = await axios.post(
+        backendUrl + "/api/image/generate-image",
+        { prompt },
+        { headers: { token } }
+      );
+      // console.log(backendUrl + "/api/user/generate-image");
+
+      if (data.success) {
+        loadCreditsData();
+        return data.resultImage;
+      } else {
+        toast.error(data.message);
+        loadCreditsData();
+        if (data.creditBalance === 0) {
+          navigate("/buy");
+        }
+      }
+    } catch (error) {
+      toast.error(error.message);
+    }
+  };
+
+  const logout = () => {
+    localStorage.removeItem("token");
+    setToken("");
+    setUser(null);
+    toast.success("Logged out");
+  };
+
+  useEffect(() => {
+    if (token) {
+      loadCreditsData();
+    }
+  }, [token]);
+
+  const value = {
+    user,
+    setUser,
+    showLogin,
+    setShowLogin,
+    backendUrl,
+    token,
+    setToken,
+    credit,
+    setCredit,
+    logout,
+    generateImage,
+    loadCreditsData,
+  };
+
+  return (
+    <AppContext.Provider value={value}>{props.children}</AppContext.Provider>
+  );
+};
+
+export default AppContextProvider;
